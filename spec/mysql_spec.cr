17fa8ddaa7f57931afcdccbb4c0dc29036ba4cde
Alexey Fedorov <waterlink000@gmail.com>
2015-03-15 02:36:34 +0200

    cover #client_info

--- spec/mysql_spec.cr
+++ spec/mysql_spec.cr
@@ -1,9 +1,23 @@
 require "./spec_helper"
 
 describe MySQL do
+  described_class = -> {
+    MySQL
+  }
+
+  subject = -> {
+    described_class.call.new
+  }
+
   describe "#initialize & .new" do
     it "creates instance of MySQL" do
-      (MySQL.new).should be_a(MySQL)
+      subject.call.should be_a(MySQL)
+    end
+  end
+
+  describe "#client_info" do
+    it "returns version" do
+      subject.call.client_info.should match(%r{^\d+\.\d+\.\d+$})
     end
   end
 end
